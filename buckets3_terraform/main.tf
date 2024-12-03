8342a8c96f4ec1e19b6bca59154df85c11bc96cb
Javier Valverde <javier.valverde.dev@gmail.com>
2024-12-02 21:04:52 -0500

    fix: deprecated directives

--- buckets3_terraform/main.tf
+++ buckets3_terraform/main.tf
@@ -12,14 +12,27 @@ provider "aws" {
   region     = "us-west-1"
 }
 
-resource "aws_s3_bucket" "bucket_demo" {
-  bucket = "bucket_demo"
+resource "aws_s3_bucket" "bucket_example" {
+  bucket = "bucket-example-${terraform.workspace}-12345"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.bucket_example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "acl_example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.bucket_example.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_object" "object1" {
+resource "aws_s3_object" "object" {
   for_each = fileset("uploads/", "*")
-  bucket   = aws_s3_bucket.bucket_demo.id
+  bucket   = aws_s3_bucket.bucket_example.id
   key      = each.value
   source   = "uploads/${each.value}"
   etag     = filemd5("uploads/${each.value}")
